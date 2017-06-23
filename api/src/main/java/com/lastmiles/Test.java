f741108c0a3caef0eb13aa0c5d42dae0d3e65b95
trehak <trehak@atlascon.cz>
2017-06-24 00:33:35 +0200

    spring boot start

--- api/src/main/java/com/lastmiles/Test.java
+++ api/src/main/java/com/lastmiles/Test.java
@@ -1,15 +1,11 @@
 package com.lastmiles;
 
-import io.test;
-
-import java.util.function.Consumer;
-
 /**
  * Created by trehak on 23.6.17.
  */
 public class Test {
 
-    public static void main(String[] args) throws Exception {
+//    public static void main(String[] args) throws Exception {
 //        KafkaEventsService eventsService = new KafkaEventsService("ec2-35-158-118-82.eu-central-1.compute.amazonaws.com:9092");
 //
 //        // THIS IS HOW YOU ATTACH LISTENER
@@ -23,6 +19,6 @@ public class Test {
 //        // THIS IS HOW YOU PRODUCE SHIT
 //        eventsService.produce(new test().setTest(" test message "));
 
-    }
+//    }
 
 }
