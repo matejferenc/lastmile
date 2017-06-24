ae0581d47918fb1a39ba2aff7b109214b79520c0
Ondrej Pospisil <pospisil.ondrej@gmail.com>
2017-06-24 04:49:38 +0200

    post offer

--- transport-service/src/main/java/com/lastmile/transport/resource/TransportResource.java
+++ transport-service/src/main/java/com/lastmile/transport/resource/TransportResource.java
@@ -1,14 +1,12 @@
 package com.lastmile.transport.resource;
 
 import com.lastmile.transport.service.TransportService;
+import com.lastmiles.TransferOffer;
 import com.lastmiles.TransferRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.inject.Singleton;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -38,5 +36,13 @@ public class TransportResource {
     @Produces({MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_JSON})
     public TransferRequest getRequest(@PathParam("id") String id) {
         return transportService.getRequest(id);
+    }
+
+    @Path("/offers")
+    @POST
+    @Produces({MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_JSON})
+    public TransferOffer postOffer(TransferOffer transferOffer) {
+        return transportService.postOffer(transferOffer);
     }
 }
--- transport-service/src/main/java/com/lastmile/transport/service/TransportService.java
+++ transport-service/src/main/java/com/lastmile/transport/service/TransportService.java
@@ -1,14 +1,19 @@
 package com.lastmile.transport.service;
 
 import com.lastmile.KafkaEventsService;
+import com.lastmile.transport.utils.UuidGen;
+import com.lastmiles.TransferOffer;
 import com.lastmiles.TransferRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by ondrej on 24.6.17.
@@ -33,5 +38,16 @@ public class TransportService {
 
     public TransferRequest getRequest(String id) {
         return requestsStorageMap.get(id);
+    }
+
+    public TransferOffer postOffer(TransferOffer transferOffer) {
+        transferOffer.setOfferId(UuidGen.generateUUID());
+        try {
+            kafkaEventsService.produce(transferOffer);
+            return transferOffer;
+        } catch (IOException e) {
+            Logger.getLogger(TransportService.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
     }
 }
