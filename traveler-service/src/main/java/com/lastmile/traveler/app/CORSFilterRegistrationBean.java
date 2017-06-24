a35d934d1f37371447b5e6113cb463c8895c1ff9
trehak <trehak@atlascon.cz>
2017-06-24 09:14:06 +0200

    CORS filter

---
+++ traveler-service/src/main/java/com/lastmile/traveler/app/CORSFilterRegistrationBean.java
@@ -0,0 +1,43 @@
+package com.lastmile.traveler.app;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * CORS filter bean
+ */
+@Component
+public class CORSFilterRegistrationBean extends FilterRegistrationBean {
+
+    public CORSFilterRegistrationBean() {
+        super(new CorsFilter(buildCfg()));
+    }
+
+    private static UrlBasedCorsConfigurationSource buildCfg() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+
+}
